2a38d95938e389da952e4195eaa66d3bf8a0a238
Magomeds05 <yusypovm@gmail.com>
2024-10-22 19:14:21 +0300

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -1,56 +1,50 @@
 import sqlite3
 
-connection = sqlite3.connect("module 14/initiate.db")
+connection = sqlite3.connect("module 14/initiate2.db")
 curs = connection.cursor()
 
-curs.execute('''
-CREATE TABLE IF NOT EXISTS Product(
-id INT,
-title TEXT NOT NULL,
-description TEXT,
-price INT NOT NULL
-)
-''')
 
+def initiate_db():
+    curs.execute('''CREATE TABLE IF NOT EXISTS Users(
+    id INT PRIMACY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INT NOT NULL,
+    balance INT NOT NULL)
+    ''')
+    curs.execute('''
+    CREATE TABLE IF NOT EXISTS Product(
+    id INT PRIMACY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INT NOT NULL)
+    ''')
+    connection.commit()
+
+
+initiate_db()
+
+curs.execute('DELETE FROM Product')
 for i in range(1, 5):
-    curs.execute("INSERT INTO Product (id, title, description, price ) VALUES (?, ?, ?, ?)", (f"{i}", f"Продукт{i}", f"Описание{i}", f"{i * 100}"))
+    curs.execute("INSERT INTO Product (id, title, description, price ) VALUES (?, ?, ?, ?)",
+                 (f"{i}", f"Продукт{i}", f"Описание{i}", f"{i * 100}"))
+    connection.commit()
+
 
 def get_all_products(id, title, description, price):
     full = [id, title, description, price]
     return full
 
-connection = sqlite3.connect("module 14/initiate.db")
-curs = connection.cursor()
-
-curs.execute('''
-CREATE TABLE IF NOT EXISTS Users(
-id INT PRIMACY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INT NOT NULL,
-balance INT NOT NULL
-)
-''')
 
 def add_user(username, email, age):
-    curs.execute(f'INSERT INTO Users VALUES({username},{email},{age}, {1000})')
+    curs.execute('INSERT INTO Users (username, email, age, balance) VALUES(?, ?, ?, 1000)', (username, email, age))
+    connection.commit()
+
 
 def is_included(username):
-    only_username = curs.execute('INSERT INTO Users (username) VALUES(?)', (f'{username}',))
-    if only_username.fetchone() is None:
-        return False
-
-
-connection.commit()
-connection.close()
-
-
-
-
-    #if check_user.fetchone() is None:
-        #curs.execute(f'''
-    #INSERT INTO Users VALUES('{username}','{email}f','{age}', 1000)
-    #''')
-    #else:
-        #return False
-    #connection.commit()
+    curs.execute('SELECT * FROM Users')
+    users = curs.fetchall()
+    for i in users:
+        if i[1] == username:
+            return True
+    return False
